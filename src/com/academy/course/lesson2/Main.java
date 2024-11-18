3c79d010d399259e16be9e2900af6a4fa4040a61
Alexandr150720 <krasnov0707052013@mail.ru>
2024-11-18 18:11:34 +0300

    Creat .gitignore and README file

---
+++ src/com/academy/course/lesson2/Main.java
@@ -0,0 +1,9 @@
+package com.academy.course.lesson2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+}
---
+++ src/com/academy/course/lesson3/Main.java
@@ -0,0 +1,8 @@
+package com.academy.course.lesson3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
