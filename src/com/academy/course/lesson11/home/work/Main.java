bcc892222d8894e38fe015145590147043ef53ed
Alexandr150720 <krasnov0707052013@mail.ru>
2024-12-21 19:03:52 +0300

    Lesson 11 with generic

--- src/com/academy/course/lesson11/home/work/Main.java
+++ src/com/academy/course/lesson11/home/work/Main.java
@@ -2,8 +2,7 @@ package com.academy.course.lesson11.home.work;
 
 import com.academy.course.lesson11.home.work.factory.BodyFactory;
 import com.academy.course.lesson11.home.work.factory.HeadFactory;
-import com.academy.course.lesson11.home.work.part.IBody;
-import com.academy.course.lesson11.home.work.part.IHead;
+import com.academy.course.lesson11.home.work.part.*;
 
 public class Main {
 
@@ -16,15 +15,15 @@ public class Main {
         IHead head2 = HeadFactory.HUAWEI.build();
         IHead head3 = HeadFactory.XIAOMI.build();
 
-        Robot robot1 = new Robot("Optimus Prime", body1, head1);
-        Robot robot2 = new Robot("Bumblebee", body1, head2);
-        Robot robot3 = new Robot("Megatron", body1, head3);
-        Robot robot4 = new Robot("Starscream", body2, head1);
-        Robot robot5 = new Robot("Ironhide", body2, head2);
-        Robot robot6 = new Robot("Galvatron", body2, head3);
-        Robot robot7 = new Robot("Shockwave", body3, head1);
-        Robot robot8 = new Robot("Mirage", body3, head2);
-        Robot robot9 = new Robot("Wheeljack", body3, head3);
+        Robot<IBody, IHead> robot1 = new Robot<>("Optimus Prime", body1, head1);
+        Robot<IBody, IHead> robot2 = new Robot<>("Bumblebee", body1, head2);
+        Robot<IBody, IHead> robot3 = new Robot<>("Megatron", body1, head3);
+        Robot<IBody, IHead> robot4 = new Robot<>("Starscream", body2, head1);
+        Robot<IBody, IHead> robot5 = new Robot<>("Ironhide", body2, head2);
+        Robot<IBody, IHead> robot6 = new Robot<>("Galvatron", body2, head3);
+        Robot<IBody, IHead> robot7 = new Robot<>("Shockwave", body3, head1);
+        Robot<IBody, IHead> robot8 = new Robot<>("Mirage", body3, head2);
+        Robot<IBody, IHead> robot9 = new Robot<>("Wheeljack", body3, head3);
 
         Robot[] transformers = new Robot[9];
         transformers[0] = robot1;
--- src/com/academy/course/lesson11/home/work/Robot.java
+++ src/com/academy/course/lesson11/home/work/Robot.java
@@ -3,13 +3,13 @@ package com.academy.course.lesson11.home.work;
 import com.academy.course.lesson11.home.work.part.IBody;
 import com.academy.course.lesson11.home.work.part.IHead;
 
-public class Robot {
+public class Robot<T, U> {
 
     private String name;
-    private IBody body;
-    private IHead head;
+    private T body;
+    private U head;
 
-    public Robot(String name, IBody body, IHead head) {
+    public Robot(String name, T body, U head) {
         this.name = name;
         this.body = body;
         this.head = head;
